ef7273d57cebddae75a68361ea7ca75f11a9e63f
Roberto Ramirez <38079022+rbtote@users.noreply.github.com>
2019-11-15 15:19:13 -0600

    get and post in heroku added

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/roberto-blog-post";
 exports.PORT = process.env.PORT || 8080;
+//mongodb+srv://rbtote:Batiz123@cluster0-pjauz.mongodb.net/roberto-blog-post?retryWrites=true&w=majority
